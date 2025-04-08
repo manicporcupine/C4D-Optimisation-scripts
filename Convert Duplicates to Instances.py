01b9486da161057609bd3b76995a3b935be49c0f
manicporcupine <111443825+manicporcupine@users.noreply.github.com>
2025-04-08 10:52:35 +0300

    Update Convert Duplicates to Instances.py

--- Convert Duplicates to Instances.py
+++ Convert Duplicates to Instances.py
@@ -16,24 +16,40 @@ def clear_selection(doc):
     for obj in all_objs:
         obj.DelBit(c4d.BIT_ACTIVE)
 
+def is_supported_type(obj):
+    return obj and obj.CheckType(c4d.Opolygon) or obj.CheckType(c4d.Ospline)
+
 def objects_are_identical(obj1, obj2):
-    if not (obj1 and obj2):
-        return False
-    if not (obj1.CheckType(c4d.Opolygon) and obj2.CheckType(c4d.Opolygon)):
+    if not obj1 or not obj2:
         return False
 
-    if obj1.GetPolygonCount() != obj2.GetPolygonCount():
-        return False
-    if obj1.GetPointCount() != obj2.GetPointCount():
+    if obj1.GetType() != obj2.GetType():
         return False
 
-    points1 = [obj1.GetPoint(i) for i in range(obj1.GetPointCount())]
-    points2 = [obj2.GetPoint(i) for i in range(obj2.GetPointCount())]
-    return set(points1) == set(points2)
+    if obj1.CheckType(c4d.Opolygon):
+        if obj1.GetPolygonCount() != obj2.GetPolygonCount():
+            return False
+        if obj1.GetPointCount() != obj2.GetPointCount():
+            return False
+        points1 = [obj1.GetPoint(i) for i in range(obj1.GetPointCount())]
+        points2 = [obj2.GetPoint(i) for i in range(obj2.GetPointCount())]
+        return set(points1) == set(points2)
 
+    elif obj1.CheckType(c4d.Ospline):
+        if obj1.GetSegmentCount() != obj2.GetSegmentCount():
+            return False
+        if obj1.GetPointCount() != obj2.GetPointCount():
+            return False
+        if obj1[c4d.SPLINEOBJECT_TYPE] != obj2[c4d.SPLINEOBJECT_TYPE]:
+            return False
+        points1 = [obj1.GetPoint(i) for i in range(obj1.GetPointCount())]
+        points2 = [obj2.GetPoint(i) for i in range(obj2.GetPointCount())]
+        return set(points1) == set(points2)
 
-def get_master_polygon(obj):
-    while obj and not obj.CheckType(c4d.Opolygon):
+    return False
+
+def get_master_object(obj):
+    while obj and not is_supported_type(obj):
         if obj.CheckType(c4d.Oinstance):
             obj = obj[c4d.INSTANCEOBJECT_LINK]
         else:
@@ -55,7 +71,7 @@ def relink_instances(doc, all_objects, master):
     for obj in all_objects:
         if obj.CheckType(c4d.Oinstance):
             ref = obj[c4d.INSTANCEOBJECT_LINK]
-            real_master = get_master_polygon(ref)
+            real_master = get_master_object(ref)
             if real_master and objects_are_identical(master, real_master):
                 doc.AddUndo(c4d.UNDOTYPE_CHANGE, obj)
                 obj[c4d.INSTANCEOBJECT_LINK] = master
@@ -63,29 +79,30 @@ def relink_instances(doc, all_objects, master):
 
 def replace_duplicates(doc, all_objects, master, other_masters):
     for obj in list(all_objects):
-        if obj.CheckType(c4d.Opolygon) and obj != master and obj not in other_masters:
-            if objects_are_identical(master, obj):
-                parent = obj.GetUp()
-                world_mtx = obj.GetMg()
-                local_mtx = ~parent.GetMg() * world_mtx if parent else world_mtx
+        if obj == master or obj in other_masters:
+            continue
+        if is_supported_type(obj) and objects_are_identical(master, obj):
+            parent = obj.GetUp()
+            world_mtx = obj.GetMg()
+            local_mtx = ~parent.GetMg() * world_mtx if parent else world_mtx
 
-                instance = c4d.BaseObject(c4d.Oinstance)
-                instance[c4d.INSTANCEOBJECT_LINK] = master
-                instance.SetName(master.GetName() + "_instance")
-                instance.SetMl(local_mtx)
-                doc.InsertObject(instance, parent=parent, pred=obj)
+            instance = c4d.BaseObject(c4d.Oinstance)
+            instance[c4d.INSTANCEOBJECT_LINK] = master
+            instance.SetName(master.GetName() + "_instance")
+            instance.SetMl(local_mtx)
+            doc.InsertObject(instance, parent=parent, pred=obj)
 
-                transfer_children(doc, obj, instance)
+            transfer_children(doc, obj, instance)
 
-                tag = obj.GetFirstTag()
-                while tag:
-                    if tag.CheckType(c4d.Ttexture):
-                        instance.InsertTag(tag.GetClone())
-                    tag = tag.GetNext()
+            tag = obj.GetFirstTag()
+            while tag:
+                if tag.CheckType(c4d.Ttexture):
+                    instance.InsertTag(tag.GetClone())
+                tag = tag.GetNext()
 
-                doc.AddUndo(c4d.UNDOTYPE_NEW, instance)
-                doc.AddUndo(c4d.UNDOTYPE_DELETE, obj)
-                obj.Remove()
+            doc.AddUndo(c4d.UNDOTYPE_NEW, instance)
+            doc.AddUndo(c4d.UNDOTYPE_DELETE, obj)
+            obj.Remove()
 
 def main():
     doc = c4d.documents.GetActiveDocument()
@@ -93,10 +110,10 @@ def main():
         gui.MessageDialog("No active document found.")
         return
 
-    selected = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_NONE)
-    masters = [obj for obj in selected if obj.CheckType(c4d.Opolygon)]
+    selection = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_NONE)
+    masters = [obj for obj in selection if is_supported_type(obj)]
     if not masters:
-        gui.MessageDialog("Please select one or more polygonal objects.")
+        gui.MessageDialog("Please select one or more polygon or spline objects.")
         return
 
     if any(m.GetDown() for m in masters):
